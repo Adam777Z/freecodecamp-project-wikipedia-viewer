907f6adbf5a88fb330a757e21d812d2166ff6394
Adam777 <Adam777-5867@outlook.com>
2018-12-27 18:25:10 +0000

    Design updated

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 		offset = 0
 		searchWikipedia();
 	});
-	
+
 	$('#search_pagination .previous').click(function() {
 		if (offset != 0) {
 			offset -= 10;
 			searchWikipedia();
 		}
 	});
-	
+
 	$('#search_pagination .next').click(function() {
 		offset += 10;
 		searchWikipedia();
@@ -20,19 +20,19 @@ $(document).ready(function() {
 var offset = 0;
 
 function searchWikipedia() {
-  $.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&prop=extracts&exsentences=1&exintro&explaintext&exlimit=max&gsroffset='+offset+'&gsrsearch='+$('input[name="search"]').val()+'&callback=?', function(data) {
+	$.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&prop=extracts&exsentences=1&exintro&explaintext&exlimit=max&gsroffset=' + offset + '&gsrsearch=' + $('input[name="search"]').val() + '&callback=?', function(data) {
 		var searchResults = $('#search_results');
-		searchResults.html('<h3 class="mt0 mb10">Results:</h3>');
+		searchResults.html('<h3 class="mb-2">Results:</h3>');
 		$.each(data['query']['pages'], function(index, value) {
-			searchResults.append('<div class="list-group mb10"><a class="list-group-item" href="https://en.wikipedia.org/?curid='+value['pageid']+'" target="_blank"><h4 class="list-group-item-heading">'+value['title']+'</h4><p class="list-group-item-text">'+value['extract']+'</p></a></div>');
+			searchResults.append('<div class="list-group mb-2"><a class="list-group-item" href="https://en.wikipedia.org/?curid=' + value['pageid'] + '" target="_blank"><h4 class="list-group-item-heading">' + value['title'] + '</h4><p class="list-group-item-text">' + value['extract'] + '</p></a></div>');
 		});
-		
+
 		if (offset == 0) {
 			$('#search_pagination .previous').addClass('disabled');
 		} else {
 			$('#search_pagination .previous').removeClass('disabled');
 		}
-		
+
 		$('#search_pagination').show();
-  });
+	});
 }
