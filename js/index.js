397c4452a22140aaaf175a5ddb2f1a2440df4bb7
Adam777 <Adam777-5867@outlook.com>
2022-10-05 21:23:32 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-	$('#search_submit').click(function() {
+	$('#search-submit').click(function() {
 		offset = 0
 		searchWikipedia();
 	});
 
-	$('#search_pagination .previous').click(function() {
+	$('#search-pagination .previous').click(function() {
 		if (offset != 0) {
 			offset -= 10;
 			searchWikipedia();
 		}
 	});
 
-	$('#search_pagination .next').click(function() {
+	$('#search-pagination .next').click(function() {
 		offset += 10;
 		searchWikipedia();
 	});
@@ -21,18 +21,18 @@ var offset = 0;
 
 function searchWikipedia() {
 	$.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&prop=extracts&exsentences=1&exintro&explaintext&exlimit=max&gsroffset=' + offset + '&gsrsearch=' + $('input[name="search"]').val() + '&callback=?', function(data) {
-		var searchResults = $('#search_results');
+		var searchResults = $('#search-results');
 		searchResults.html('<h3 class="mb-2">Results:</h3>');
 		$.each(data['query']['pages'], function(index, value) {
 			searchResults.append('<div class="list-group mb-2"><a class="list-group-item" href="https://en.wikipedia.org/?curid=' + value['pageid'] + '" target="_blank"><h4 class="list-group-item-heading">' + value['title'] + '</h4><p class="list-group-item-text">' + value['extract'] + '</p></a></div>');
 		});
 
 		if (offset == 0) {
-			$('#search_pagination .previous').addClass('disabled');
+			$('#search-pagination .previous').addClass('disabled');
 		} else {
-			$('#search_pagination .previous').removeClass('disabled');
+			$('#search-pagination .previous').removeClass('disabled');
 		}
 
-		$('#search_pagination').show();
+		$('#search-pagination').show();
 	});
 }
