9a7f126d8c1ec06d2d7ad41a4f2dea5d7b9b8fa9
Adam777 <Adam777-5867@outlook.com>
2022-10-06 18:11:56 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -6,14 +6,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		search_wikipedia();
 	});
 
-	document.querySelector('#search-pagination .previous').addEventListener('click', (event) => {
-		event.preventDefault();
-
+	document.querySelector('#search-pagination-previous').addEventListener('click', (event) => {
 		if (offset != 0) {
 			offset -= 10;
 			search_wikipedia();
 
-			window.scrollTo({
+			window.scroll({
 				top: 0,
 				left: 0,
 				behavior: 'smooth'
@@ -21,13 +19,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		}
 	});
 
-	document.querySelector('#search-pagination .next').addEventListener('click', (event) => {
-		event.preventDefault();
-
+	document.querySelector('#search-pagination-next').addEventListener('click', (event) => {
 		offset += 10;
 		search_wikipedia();
 
-		window.scrollTo({
+		window.scroll({
 			top: 0,
 			left: 0,
 			behavior: 'smooth'
@@ -37,11 +33,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function search_wikipedia() {
 	if (document.querySelector('input[name="search"]').reportValidity()) {
-		fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&generator=search&prop=extracts&exsentences=1&exintro&explaintext&exlimit=max&gsroffset=' + offset + '&gsrsearch=' + document.querySelector('input[name="search"]').value, {
+		fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&generator=search&prop=extracts|info&inprop=url&exsentences=1&exintro&explaintext&exlimit=max&gsroffset=' + offset + '&gsrsearch=' + document.querySelector('input[name="search"]').value, {
 			'method': 'GET'
 		})
 		.then((response) => {
-			if (response.ok) {
+			if (response['ok']) {
 				return response.json();
 			} else {
 				throw 'Error';
@@ -54,13 +50,13 @@ function search_wikipedia() {
 				searchResults.innerHTML = '<h3 class="mb-2">Results:</h3>';
 
 				for (let [key, value] of Object.entries(data['query']['pages'])) {
-					searchResults.innerHTML += '<div class="list-group mb-2"><a class="list-group-item" href="https://en.wikipedia.org/?curid=' + value['pageid'] + '" target="_blank"><h4 class="list-group-item-heading">' + value['title'] + '</h4><p class="list-group-item-text">' + value['extract'] + '</p></a></div>';
+					searchResults.innerHTML += '<div class="list-group mb-2"><a class="list-group-item" href="' + value['fullurl'] + '" target="_blank"><h4 class="list-group-item-heading">' + value['title'] + '</h4><p class="list-group-item-text">' + value['extract'] + '</p></a></div>';
 				}
 
 				if (offset == 0) {
-					document.querySelector('#search-pagination .previous').classList.add('disabled');
+					document.querySelector('#search-pagination-previous').classList.add('disabled');
 				} else {
-					document.querySelector('#search-pagination .previous').classList.remove('disabled');
+					document.querySelector('#search-pagination-previous').classList.remove('disabled');
 				}
 
 				document.querySelector('#search-pagination').style.display = 'block';
